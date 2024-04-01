4f11b0ea47c6d101d8b69db94e94e6dd1d90e065
mateo.dischler <mateo.dischler@student-cs.fr>
2024-04-01 11:06:48 +0200

    changed way we add vibrato

--- TL1.m
+++ TL1.m
@@ -16,8 +16,8 @@ filename = 'signal_harmonique.wav';
 audiowrite(filename, signal, sampling_freq);
 
 % Lire le fichier .wav pour vérifier le son
-[y, fs] = audioread(filename);
-sound(y, fs);
+% [y, fs] = audioread(filename);
+% sound(y, fs);
 
 % Analyse du signal
 % Vous pouvez utiliser la transformée de Fourier pour analyser les fréquences du signal
@@ -30,27 +30,26 @@ xlabel('Fréquence (Hz)');
 ylabel('Amplitude');
 title('Transformée de Fourier du signal');
 
-%%Question 2
+%% Question 2
 
 % Paramètres du vibrato
 vibrato_amplitude = 20; % Amplitude du vibrato en Hz
 vibrato_frequency = 5; % Fréquence du vibrato en Hz
 
-% Générer le signal vibrato
-vibrato = vibrato_amplitude * sin(2 * pi * vibrato_frequency * t);
+% Générer l'onde sinusoïdale pour le vibrato
+vibrato_wave = vibrato_amplitude * sin(2 * pi * vibrato_frequency * t);
 
 % Ajouter le vibrato au signal de départ
-signal_with_vibrato = sin(2 * pi * (fundamental_freq + vibrato) .* t);
+vibrato_signal = signal .* (1 + vibrato_wave);
 
 % Écrire le signal avec vibrato dans un fichier .wav
-filename_with_vibrato = 'signal_with_vibrato.wav';
-audiowrite(filename_with_vibrato, signal_with_vibrato, sampling_freq);
+vibrato_filename = 'signal_harmonique_vibrato.wav';
+audiowrite(vibrato_filename, vibrato_signal, sampling_freq);
 
-% Lire le fichier .wav pour vérifier le son avec vibrato
-[y_with_vibrato, fs_with_vibrato] = audioread(filename_with_vibrato);
-sound(y_with_vibrato, fs_with_vibrato);
+% Afficher le spectrogramme pour observer le vibrato
+spectrogram(vibrato_signal, 256, 250, [], sampling_freq, 'yaxis');
+title('Spectrogramme du signal avec vibrato');
 
-% Afficher le spectrogramme du signal avec vibrato pour vérification
-figure;
-spectrogram(signal_with_vibrato, hamming(256), 128, 256, sampling_freq, 'yaxis');
+% Afficher le spectre avec pspectrum
+pspectrum(vibrato_signal, sampling_freq, 'spectrogram', 'OverlapPercent', 0, 'FrequencyLimits', [0, 2000]);
 title('Spectrogramme du signal avec vibrato');
